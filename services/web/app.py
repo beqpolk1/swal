968ff24e38886015beccc482ea9e9797c74e95e2
beqpolk1 <polk.ben@gmail.com>
2024-12-28 00:04:57 -0700

    Add PyMongo and sample DB test route

--- services/web/app.py
+++ services/web/app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://db:27017/"
+
+mongoClient = MongoClient(app.config["MONGO_URI"])
 
 @app.route('/')
 def hello_world():
-    return 'Hello World - this is Flasky McFlask Face'
+    return 'Hello World - this is Flasky McFlask Face<br />2'
+
+@app.route('/dbtest')
+def dbtest():
+    mongoClient.admin.command('ping')
+    return 'Connected to MongoDB version ' + mongoClient.server_info()["version"]
 
 if __name__ == '__main__':
     app.run()
