74f9006e0dfbf6ae7ac6bd9ca591fe79e59970c7
beqpolk1 <polk.ben@gmail.com>
2025-01-24 20:21:43 -0700

    Add comments/file organization

--- services/web/app.py
+++ services/web/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, redirect, url_for
 from pymongo import MongoClient
 
+################################################
+# app startup/config
+################################################
+
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://db:27017/"
 app.config["MEDIA_IMG"] = "img/"
 
 mongoClient = MongoClient(app.config["MONGO_URI"])
+
+################################################
+# basic/utility routes
+################################################
 
 @app.route('/media/<filename>')
 def media():
@@ -27,6 +35,10 @@ def statictest():
 @app.route('/mediatest')
 def mediatest():
     return redirect(url_for('media', filename = app.config["MEDIA_IMG"] + 'media_test.jpg'))
+
+################################################
+# external module routes
+################################################
 
 if __name__ == '__main__':
     app.run()
