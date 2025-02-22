52ae4bcd8698b9b209f08f4e5e1ca8cf03ddb01a
beqpolk1 <polk.ben@gmail.com>
2025-02-22 11:35:34 -0700

    Add perform_add_entry code, add call from add_entry module, update add_entry template

--- services/web/modules/add_entry.py
+++ services/web/modules/add_entry.py
@@ -1,18 +1,11 @@
 from flask import render_template, request
-from .entry import Entry
 from .perform_add_entry import perform_add_entry
+from classes import Job_Status
 
 def add_entry(mode : str):
     if (request.method == "GET"): 
         return render_template("add_entry.html.j2", mode = mode)
+    
     elif (request.method == "POST"):
-        new_entry = Entry(request.form["artist"], 
-                          request.form["album"], 
-                          request.form["release_year"], 
-                          request.form["genre"], 
-                          request.form["interest_level"], 
-                          request.form["is_starred"], 
-                          request.form["is_obtained"], 
-                          request.form["link"])
-        
-        return perform_add_entry(new_entry)
+        add_result = perform_add_entry(request.form)
+        return render_template("add_entry.html.j2", result = add_result)
