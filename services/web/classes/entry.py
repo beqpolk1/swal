b02f4760f660039f3e46f7ef15c416ce7899c2b0
beqpolk1 <polk.ben@gmail.com>
2025-03-09 21:14:56 -0600

    Add artwork fields to Entry

--- services/web/classes/entry.py
+++ services/web/classes/entry.py
@@ -7,6 +7,9 @@ class Entry:
         self.interest = new_vals.get("interest_level")
         self.starred = new_vals.get("is_starred")
         self.obtained = new_vals.get("is_obtained")
+        self.link = new_vals.get("link") or None
+        self.artwork_file_name = new_vals.get("artwork_file") or None
+        self.artwork_file = new_files.get("artwork_file")
 
     def __str__(self):
         all_attr = {
@@ -16,6 +19,8 @@ class Entry:
             "genre": self.genre,
             "interest_level": self.interest,
             "is_starred": self.starred,
+            "artwork_file_name": self.artwork_file_name,
+            "artwork_file": True if (self.artwork_file) else False,
             "is_obtained": self.obtained,
             "link": self.link
         }
