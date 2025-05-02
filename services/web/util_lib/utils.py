bb9abd35056738abdcad17f864f1199fdd14976f
beqpolk1 <polk.ben@gmail.com>
2025-05-02 11:56:54 -0500

    Add utils library

---
+++ services/web/util_lib/utils.py
@@ -0,0 +1,24 @@
+def str_to_bool(val):
+    if isinstance(val, bool):
+        return val
+    
+    if isinstance(val, str):
+        val = val.strip().lower()
+        if val in ("true", "1", "yes", "on"):
+            return True
+        elif val in ("false", "0", "no", "off"):
+            return False
+        
+    raise ValueError(f"Invalid boolean value: {val}")
+
+def get_str_or_none(d: dict, key: str):
+    val = d.get(key) or None
+    return str(val) if val not in ["", None] else None
+
+def get_int_or_none(d: dict, key: str):
+    val = d.get(key) or None
+    return int(val) if val is not None else None
+
+def get_bool_or_none(d: dict, key: str):
+    val = d.get(key) or None
+    return str_to_bool(val) if val is not None else None
