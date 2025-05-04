66658155fd2dc701f6714931aee0a13fa45b6726
beqpolk1 <polk.ben@gmail.com>
2025-05-04 11:26:02 -0500

    Add exception handling to module-level db connection in db_interface

--- services/web/modules/db_interface.py
+++ services/web/modules/db_interface.py
@@ -1,11 +1,27 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 from classes import Entry
 
-mongo_client = MongoClient("mongodb://mongoadmin:secret@db:27017/")
-db = mongo_client["swal"]
+_db = None
+
+def get_db():
+    global _db
+
+    if _db is None:
+        try:
+            mongo_client = MongoClient("mongodb://mongoadmin:secret@db:27017/")
+            mongo_client.admin.command("ping")
+            _db = mongo_client["swal"]
+        except errors.PyMongoErrors as e:
+            raise ConnectionError(f"Could not connect to MongoDB: {e}")
+        except:
+            raise ConnectionError(f"Unknown error connecting to MongoDB: {e}")
+    
+    return _db
+
 
 def add_entry_to_db(new_entry : Entry):
-    entries = db["entries"]
+    db_conn = get_db()
+    entries = db_conn["entries"]
 
     upl_entry = new_entry.to_dict()
     if ("_id" in upl_entry and upl_entry["_id"] is None): del upl_entry["_id"]
