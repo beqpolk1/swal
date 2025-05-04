c2bf37b626ea1d19c8ab419a945a12fb6da7592e
beqpolk1 <polk.ben@gmail.com>
2025-05-04 10:53:10 -0500

    Add more granular updates to result.status when adding entry

--- services/web/modules/perform_add_entry.py
+++ services/web/modules/perform_add_entry.py
@@ -12,11 +12,11 @@ def perform_add_entry(form_data, files_data) -> Job_Status:
         result.status = "entry built"
 
         if (new_entry.artwork_file):
-            new_entry.artwork_file_name = _perform_artwork_add(new_entry.artwork_file_name, new_entry.artwork_file)
+            new_entry.artwork_file_name = _perform_artwork_add(new_entry.artwork_file_name, new_entry.artwork_file, result)
             result.status = "artwork uploaded"
 
         new_entry_id = _perform_db_add(new_entry)
-        result.status = new_entry_id
+        result.status = "added to DB"
         result.new_img = new_entry.artwork_file_name
         result.success = True
     else:
@@ -24,20 +24,23 @@ def perform_add_entry(form_data, files_data) -> Job_Status:
 
     return result
 
-def _perform_artwork_add(filename : str, file) -> str:
+def _perform_artwork_add(filename : str, file, result : Job_Status) -> str:
     new_filename = str(uuid.uuid4()) + filename[filename.rfind("."):]
     new_filepath = os.path.join('/var/tmp', new_filename)
 
     file.save(new_filepath)
+    result.status = "artwork file saved locally"
 
     ftp_server = ftplib.FTP("nginx", "swal_image", "swallowth3p4ssword")
     ftp_server.cwd("img")
 
     with open(new_filepath, "rb") as upl_file:
         ftp_server.storbinary(f"STOR {new_filename}", upl_file)
-    
+        result.status = "artwork transferred to server"
     ftp_server.quit
+
     os.remove(new_filepath)
+    result.status = "local artwork file cleaned"
 
     return new_filename
 
