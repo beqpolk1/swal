5929bda65d0c5c7551658a05b134323f39af8365
beqpolk1 <polk.ben@gmail.com>
2025-05-24 12:58:39 -0500

    Adjust get_db to accept db_name parameter, add exception handling to add_entry_to_db

--- services/web/modules/db_interface.py
+++ services/web/modules/db_interface.py
@@ -3,28 +3,33 @@ from classes import Entry
 
 _db = None
 
-def get_db():
+def get_db(db_name : str):
     global _db
 
     if _db is None:
         try:
             mongo_client = MongoClient("mongodb://mongoadmin:secret@db:27017/")
             mongo_client.admin.command("ping")
-            _db = mongo_client["swal"]
         except errors.PyMongoErrors as e:
             raise ConnectionError(f"Could not connect to MongoDB: {e}")
         except:
             raise ConnectionError(f"Unknown error connecting to MongoDB: {e}")
+            _db = mongo_client[db_name]
     
     return _db
 
 
 def add_entry_to_db(new_entry : Entry):
-    db_conn = get_db()
-    entries = db_conn["entries"]
-
     upl_entry = new_entry.to_dict()
     if ("_id" in upl_entry and upl_entry["_id"] is None): del upl_entry["_id"]
 
-    new_entry_id = entries.insert_one(upl_entry).inserted_id
+    try:
+        db_conn = get_db("swal")
+        entries = db_conn["entries"]
+        new_entry_id = entries.insert_one(upl_entry).inserted_id
+    except ConnectionError as e:
+        raise e
+    except Exception as e:
+        raise Exception("Other exception adding entry to DB") from e
+    
     return new_entry_id
