0d26745bebb1161c9915aa6a3ba68cbd60646b85
beqpolk1 <polk.ben@gmail.com>
2025-05-26 22:33:39 -0500

    Add error_library module

---
+++ services/web/util_lib/error_library.py
@@ -0,0 +1,28 @@
+artist_must_be_string = "Artist name must be a string"
+artist_too_long = "Artist name must be 255 characters or less"
+artist_required = "Artist name must be present"
+
+album_must_be_string = "Album name must be a string"
+album_too_long = "Album name must be 500 characters or less"
+album_required = "Album name must be present"
+
+release_year_must_be_int = "Release year must be an integer"
+release_year_range = "Release year must be between 1900 and 2100"
+release_year_required = "Release year must be present"
+
+genre_must_be_string = "Genre must be a string"
+genre_too_long = "Genre must be 255 characters or less"
+
+interest_level_must_be_int = "Interest level must be an integer"
+interest_level_range = "Interest level must be 1, 2, or 3"
+
+starred_invalid = "Starred must be true or false"
+
+artwork_filename_must_be_string = "Artwork file name must be a string"
+artwork_file_mismatch = "Artwork file name and artwork file must both be present"
+
+obtained_invalid = "Is obtained must be true or false"
+obtained_required = "Is obtained must be present"
+
+link_must_be_string = "Link must be a string"
+link_invalid_format = "Link must be in a valid URL format"
