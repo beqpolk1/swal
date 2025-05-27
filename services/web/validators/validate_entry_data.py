afa200dfa8e6bab6efa2b3183bb571db04604ea4
beqpolk1 <polk.ben@gmail.com>
2025-05-26 22:38:57 -0500

    Refactor validate_entry_data to use error_library, have validators return strings

--- services/web/validators/validate_entry_data.py
+++ services/web/validators/validate_entry_data.py
@@ -4,123 +4,108 @@ import util_lib
 def validate_entry_data(entry_data, entry_files) -> list:
     errors = []
 
-    _validate_artist(entry_data.get("artist") or None, errors)
-    _validate_album(entry_data.get("album") or None, errors)
-    _validate_release_year(entry_data.get("release_year") or None, errors)
-    _validate_genre(entry_data.get("genre") or None, errors)
-    _validate_interest_level(entry_data.get("interest_level"), errors)
-    _validate_starred(entry_data.get("is_starred"), errors)
-    _validate_artwork(entry_files.get("artwork_file").filename or None, entry_files.get("artwork_file"), errors)
-    _validate_obtained(entry_data.get("is_obtained"), errors)
-    _validate_link(entry_data.get("link") or None, errors)
+    for func in [
+        lambda: _validate_artist(entry_data.get("artist") or None),
+        lambda: _validate_album(entry_data.get("album") or None),
+        lambda: _validate_release_year(entry_data.get("release_year") or None),
+        lambda: _validate_genre(entry_data.get("genre") or None),
+        lambda: _validate_interest_level(entry_data.get("interest_level")),
+        lambda: _validate_starred(entry_data.get("is_starred")),
+        lambda: _validate_artwork(
+            entry_files.get("artwork_file").filename or None,
+            entry_files.get("artwork_file")
+        ),
+        lambda: _validate_obtained(entry_data.get("is_obtained")),
+        lambda: _validate_link(entry_data.get("link") or None)
+    ]:
+        result = func()
+        if result:
+            errors.append(result)
 
     return errors
-    
-def _validate_artist(artist, errors):
-    if (artist not in [None, ""]):
-        if (not isinstance(artist, str)):
-            try:
-                artist = str(artist)
-            except:
-                errors.append({"code": "A01", "msg": "Artist name must be a string"})
-                return
-               
-        if (len(artist) > 255):
-            errors.append({"code": "A02", "msg": "Artist name must be 255 characters or less"})
+
+def _validate_artist(artist):
+    if artist not in [None, ""]:
+        try:
+            artist = str(artist)
+        except:
+            return util_lib.artist_must_be_string
+        if len(artist) > 255:
+            return util_lib.artist_too_long
     else:
-        errors.append({"code": "A03", "msg": "Artist name must be present"})
+        return util_lib.artist_required
 
-def _validate_album(album, errors):
-    if (album not in [None, ""]):
-        if (not isinstance(album, str)):
-            try:
-                album = str(album)
-            except:
-                errors.append({"code": "A04", "msg": "Album name must be a string"})
-                return
-            
-        if (len(album) > 500):
-            errors.append({"code": "A05", "msg": "Album name must be 500 characters or less"})
+def _validate_album(album):
+    if album not in [None, ""]:
+        try:
+            album = str(album)
+        except:
+            return util_lib.album_must_be_string
+        if len(album) > 500:
+            return util_lib.album_too_long
     else:
-        errors.append({"code": "A06", "msg": "Album name must be present"})
+        return util_lib.album_required
 
-def _validate_release_year(release_year, errors):
-    if (release_year is not None):
-        if (not isinstance(release_year, int)):
-            try:
-                release_year = int(release_year)
-            except:
-                errors.append({"code": "A07", "msg": "Release year must be an integer"})
-                return
-            
-        if (release_year < 1900 or release_year > 2100):
-            errors.append({"code": "A08", "msg": "Release year must be between 1900 and 2100"})  
+def _validate_release_year(release_year):
+    if release_year is not None:
+        try:
+            release_year = int(release_year)
+        except:
+            return util_lib.release_year_must_be_int
+        if release_year < 1900 or release_year > 2100:
+            return util_lib.release_year_range
     else:
-        errors.append({"code": "A09", "msg": "Release year must be present"})
+        return util_lib.release_year_required
 
-def _validate_genre(genre, errors):  
-    if (genre is not None):
-        if (not isinstance(genre, str)):
-            try:
-                genre = str(genre)
-            except:
-                errors.append({"code": "A10", "msg": "Genre must be a string"})
-                return
-            
-        if (len(genre) > 255):
-            errors.append({"code": "A11", "msg": "Genre must be 255 characters or less"})
+def _validate_genre(genre):
+    if genre is not None:
+        try:
+            genre = str(genre)
+        except:
+            return util_lib.genre_must_be_string
+        if len(genre) > 255:
+            return util_lib.genre_too_long
 
-def _validate_interest_level(interest_level, errors):
-    if (interest_level is not None):
-        if (not isinstance(interest_level, int)):
-            try:
-                interest_level = int(interest_level)
-            except:
-                errors.append({"code": "A12", "msg": "Interest level must be an integer"})
-                return
-            
-        if (interest_level < 1 or interest_level > 3):
-            errors.append({"code": "A13", "msg": "Interest level must be 1, 2, or 3"})
+def _validate_interest_level(interest_level):
+    if interest_level is not None:
+        try:
+            interest_level = int(interest_level)
+        except:
+            return util_lib.interest_level_must_be_int
+        if interest_level not in [1, 2, 3]:
+            return util_lib.interest_level_range
 
-def _validate_starred(starred, errors):
-    if (starred is not None):
-        if (not isinstance(starred, bool)):
-            try:
-                starred = util_lib.str_to_bool(starred)
-            except:
-                errors.append({"code": "A14", "msg": "Starred must be true or false"})
+def _validate_starred(starred):
+    if starred is not None:
+        try:
+            util_lib.str_to_bool(starred)
+        except:
+            return util_lib.starred_invalid
 
-def _validate_artwork(artwork, artwork_file, errors):
-    if (artwork is not None):
-        if (not isinstance(artwork, str)):
-            try:
-                artwork = str(artwork)
-            except:
-                errors.append({"code": "A19", "msg": "artwork file name must be a string"})
-    
-    if ((artwork is not None and not artwork_file) or (artwork is None and artwork_file)):
-        errors.append({"code": "A20", "msg": "artwork file name and artwork file must both be present"})
+def _validate_artwork(artwork, artwork_file):
+    if artwork is not None:
+        try:
+            artwork = str(artwork)
+        except:
+            return util_lib.artwork_filename_must_be_string
+    if (artwork and not artwork_file) or (not artwork and artwork_file):
+        return util_lib.artwork_file_mismatch
 
-def _validate_obtained(obtained, errors):
-    if (obtained is not None):
-        if (not isinstance(obtained, bool)):
-            try:
-                obtained = util_lib.str_to_bool(obtained)
-            except:
-                errors.append({"code": "A15", "msg": "Is obtained must be true or false"})
+def _validate_obtained(obtained):
+    if obtained is not None:
+        try:
+            util_lib.str_to_bool(obtained)
+        except:
+            return util_lib.obtained_invalid
     else:
-        errors.append({"code": "A16", "msg": "Is obtained must be present"})
+        return util_lib.obtained_required
 
-def _validate_link(link, errors):
+def _validate_link(link):
     url_pattern = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
-    
-    if (link is not None):
-        if (not isinstance(link, str)):
-            try:
-                link = str(link)
-            except:
-                errors.append({"code": "A18", "msg": "Link must be a string"})
-                return
-            
-        if (not re.fullmatch(url_pattern, link)):
-            errors.append({"code": "A17", "msg": "Link must be in a valid URL format"})
+    if link is not None:
+        try:
+            link = str(link)
+        except:
+            return util_lib.link_must_be_string
+        if not re.fullmatch(url_pattern, link):
+            return util_lib.link_invalid_format
