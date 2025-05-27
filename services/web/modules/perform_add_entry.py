ebe8aa4fdba0893062c30299d994bd718c379d70
beqpolk1 <polk.ben@gmail.com>
2025-05-26 23:04:30 -0500

    Refactor perform_add_entry to use job_status add_error method

--- services/web/modules/perform_add_entry.py
+++ services/web/modules/perform_add_entry.py
@@ -24,11 +24,13 @@ def perform_add_entry(form_data, files_data) -> Job_Status:
                 except Exception as e:
                     result.update_status("failed to process and upload artwork file")
                     raise e from e
+
             try:
                 new_entry._id = str(_perform_db_add(new_entry))
                 result.update_status("new entry added to DB")
             except Exception as e:
-                if (new_entry.artwork_file): _cleanup_artwork_file(new_entry.artwork_file_name, result)
+                if (new_entry.artwork_file):
+                    _cleanup_artwork_file(new_entry.artwork_file_name, result)
                 result.update_status("failed to add entry to database")
                 raise e from e
             
@@ -39,7 +41,7 @@ def perform_add_entry(form_data, files_data) -> Job_Status:
         else:
             result.update_status("new entry data failed validation")
     except Exception as e:
-        result.errors.append({"code": "AEX05", "msg": f"Exception adding entry: {e}"})
+        result.add_error(f"Exception adding entry: {e}")
 
     return result
 
@@ -87,13 +89,13 @@ def _perform_artwork_add(filename : str, file, result : Job_Status) -> str:
             result.update_status("local artwork file cleaned")
 
         except FileNotFoundError:
-            result.errors.append({"code": "AEX01", "msg": f"Could not find temp file {new_filepath}"})
+            result.add_error(f"Could not find temp file {new_filepath}")
         except PermissionError:
-            result.errors.append({"code": "AEX02", "msg": f"Do not have permissions to delete temp file {new_filename}"})
+            result.add_error(f"Do not have permissions to delete temp file {new_filename}")
         except OSError as e:
-            result.errors.append({"code": "AEX03", "msg": f"OSError deleting temp file {new_filepath}"})
+            result.add_error(f"OSError deleting temp file {new_filepath}")
         except Exception as e:
-            result.errors.append({"code": "AEX04", "msg": f"Ohter exception deleting temp file {new_filepath}"})
+            result.add_error(f"Other exception deleting temp file {new_filepath}")
 
     return new_filename
 
@@ -108,15 +110,15 @@ def _cleanup_artwork_file(filename : str, result : Job_Status):
         ftp_server.quit
         result.update_status("artwork removed from static server")
 
-    except (socket.gaierror, socket.timeout, ConnectionRefusedError) as e:
-        result.errors.append({"code": "AEX01", "msg": f"Network/connection error connecting to FTP server"})
+    except (socket.gaierror, socket.timeout, ConnectionRefusedError):
+        result.add_error("Network/connection error connecting to FTP server")
         result.update_status("failed to cleanup artwork from static server")
-    except ftplib.error_perm as e:
-        result.errors.append({"code": "AEX01", "msg": f"Permanent FTP error (e.g., login failed)"})
+    except ftplib.error_perm:
+        result.add_error("Permanent FTP error (e.g., login failed)")
         result.update_status("failed to cleanup artwork from static server")
-    except ftplib.all_errors as e:
-        result.errors.append({"code": "AEX01", "msg": f"General FTP error"})
+    except ftplib.all_errors:
+        result.add_error("General FTP error")
         result.update_status("failed to cleanup artwork from static server")
-    except Exception as e:
-        result.errors.append({"code": "AEX01", "msg": f"Other exception transferring artwork file"})
+    except Exception:
+        result.add_error("Other exception transferring artwork file")
         result.update_status("failed to cleanup artwork from static server")
