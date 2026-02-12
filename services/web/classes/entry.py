083a2b310d402e4b0b0cb3d2ce6d764e4c188f4f
beqpolk1 <polk.ben@gmail.com>
2026-02-12 11:52:03 -0600

    Temp fixes to Entry class to make compatible with returning search results

--- services/web/classes/entry.py
+++ services/web/classes/entry.py
@@ -9,13 +9,15 @@ class Entry:
         self.obtained = util_lib.str_to_bool(new_vals.get("is_obtained"))
 
         #optional fields
-        self._id = util_lib.get_str_or_none(new_vals, "id")
+        self._id = util_lib.get_str_or_none(new_vals, "_id")
         self.genre = util_lib.get_str_or_none(new_vals, "genre")
         self.interest = util_lib.get_int_or_none(new_vals, "interest_level")
         self.link = util_lib.get_str_or_none(new_vals, "link")
-        self.artwork_file_name = new_files.get("artwork_file").filename or None
+
         self.artwork_file = new_files.get("artwork_file")
-        self.starred = util_lib.get_bool_or_none(new_vals, "is_starred")
+        if self.artwork_file: self.artwork_file_name = new_files.get("artwork_file").filename or None
+        
+        self.is_starred = util_lib.str_to_bool(new_vals.get("is_starred"))
 
     def to_dict(self):
         return {
@@ -25,7 +27,7 @@ class Entry:
             "release_year": self.release_year,
             "genre": self.genre,
             "interest_level": self.interest,
-            "is_starred": self.starred,
+            "is_starred": self.is_starred,
             "artwork_file_name": self.artwork_file_name,
             "artwork_file": True if (self.artwork_file) else False,
             "is_obtained": self.obtained,
