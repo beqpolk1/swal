473be93c03c7271ab31b8e8b170856993efe673f
beqpolk1 <polk.ben@gmail.com>
2026-03-14 00:19:21 -0600

    Add util function to decode a base64 string and use it to populate a dictionary

--- services/web/util_lib/utils.py
+++ services/web/util_lib/utils.py
@@ -1,4 +1,5 @@
 from .error_library import *
+import base64, json
 
 def str_to_bool(val):
     if isinstance(val, bool):
@@ -45,3 +46,8 @@ def get_bool_or_none_2(d: dict, key: str):
 
     return str_to_bool(val)
 
+def base64_enc_to_dict(base64_enc: str) -> dict:
+    decoded_bytes = base64.urlsafe_b64decode(base64_enc + "===", validate=True)
+    decoded_text = decoded_bytes.decode("utf-8")
+    obj = json.loads(decoded_text)
+    return obj
