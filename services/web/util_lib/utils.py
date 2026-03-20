a2f2e025c4fdb3e448dbf01accb0f52354270d95
beqpolk1 <polk.ben@gmail.com>
2026-03-19 23:35:44 -0600

    Remove validate parameter from call to urlsafe_b64decode

--- services/web/util_lib/utils.py
+++ services/web/util_lib/utils.py
@@ -47,7 +47,7 @@ def get_bool_or_none_2(d: dict, key: str):
     return str_to_bool(val)
 
 def base64_enc_to_dict(base64_enc: str) -> dict:
-    decoded_bytes = base64.urlsafe_b64decode(base64_enc + "===", validate=True)
+    decoded_bytes = base64.urlsafe_b64decode(base64_enc + "===")
     decoded_text = decoded_bytes.decode("utf-8")
     obj = json.loads(decoded_text)
     return obj
